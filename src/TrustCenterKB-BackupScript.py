5fc4eaedd327e686f8c0ce19b8363e6c66e6e6e0
Elaine Chen <echen@conceptspring.com>
2015-10-16 17:46:25 -0400

    Fixed log file
    Made log file capture category/section/articles, cleaned up formating of
    backup

--- src/TrustCenterKB-BackupScript.py
+++ src/TrustCenterKB-BackupScript.py
@@ -6,7 +6,9 @@ Created on Oct 16, 2015
 from setuptools.package_index import Credential
 
 ''' **************** WELCOME  '''
+print('********************************************************************')
 print('Welcome to the MIT Trust Center Zendesk knowledgebase backup script.')
+print('********************************************************************')
 print('Press any key to continue...')
 input()
 
@@ -28,21 +30,27 @@ agentPassword = input('Please enter Zendesk agent password:')
 credentials = agentUsername, agentPassword
 
 ''' **************** Getting ready to pull down the backup'''
-
+''' set up session variable'''
 session = requests.Session()
 session.auth = credentials
 
+'''set up zendesk url, language'''
 zendesk = 'https://miteship.zendesk.com'
 language = 'en-US'
 
+'''pull down date, compile backup path, mkdir if not present'''
 date = datetime.date.today()
 backup_path = os.path.join(str(date), language)
 if not os.path.exists(backup_path):
   os.makedirs(backup_path)
   
-endpoint = zendesk + '/api/v2/help_center/en-us/articles.json'.format(locale=language.lower()) 
+''' set up the log'''
+log = []
+    
+''' set up the endpoint, this is what is actually used to pull down the content'''  
+endpoint = zendesk + '/api/v2/help_center/en-us/articles.json'.format(locale=language.lower()) + '?include=users,sections,categories'
 
-print('Press any key to continue...')
+print('Press any key to start the backup. This will take a few moments.')
 input()
 
 ''' **************** Posting the actual request for backup'''
@@ -64,22 +72,52 @@ while endpoint:
      data = response.json()
 
      '''Print article titles for reference'''
+     sectionID = 0
+     articleID = 0
+     categoryID = 0
+     
+     '''Iterate through articles'''
      for article in data['articles']:
-         title = '<h1>' + article['title'] + '</h1>'
-         filename = '{id}.html'.format(id=article['id'])
-         with open(os.path.join(backup_path, filename), mode='w', encoding='utf-8') as f:
-             f.write(title + '\n' + article['body'])
+         
+         ''' Get category and section ID and title '''
+         sectionName = 'null'
+         categoryName = 'null'
+         for section in data['sections']:
+             if ( section['id'] == article['section_id'] ):
+                sectionName = section['name']
+                for category in data['categories']:
+                     if ( category['id'] == section['category_id']):
+                          categoryName = category['name']
+          
+         articleName = article['title']
+         categoryTitle = '<h1>' + categoryName + '</h1>\n'                
+         sectionTitle = '<h2>' + sectionName + '</h2>\n'         
+         articleTitle = '<h3>' + articleName + '</h3>\n'
+         articleFilename = '{id}.html'.format(id=article['id'])
+             
+         with open(os.path.join(backup_path, articleFilename), mode='w', encoding='utf-8') as f:
+             f.write(categoryTitle + '\n' + sectionTitle + '\n' + articleTitle + '\n' + article['body'])
+         
+         ''' this was giving the script trouble bc command cannot display non
+             unicode characters. comment out for now until we figure out how to 
+             reformat those pesky special characters. '''
          '''articleRawTitle = '{title} copied!'.format(title=article['title'])
          print(artileRawTitle.encode('cp850', errors='replace'))'''
+         
+         ''' count how many articles we wrote to file'''
          articleCount = articleCount + 1
          print('Article #', articleCount, 'written to file')
+         log.append((articleFilename, categoryName, sectionName, articleName))
      
      '''page through the results until next page is null'''
      endpoint = data['next_page']  
 
+''' Write log to CSV'''
+with open(os.path.join(backup_path, '_log.csv'), mode='wt', encoding='utf-8') as f:
+     writer = csv.writer(f)
+     writer.writerow( ('File', 'Category', 'Section', 'Title') )
+     for article in log:
+         writer.writerow(article)
 
-'''
-'C:\CLOUDROOT\Dropbox (MIT)\MIT Stuff\TRUSTCENTERSTUFF\elaine-sandbox\zendesk-backup
-'''
      
      
