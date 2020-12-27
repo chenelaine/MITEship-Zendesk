c978c22e6ab2cedb72f7946913d85a22c2a4f1e6
Elaine Chen <echen@conceptspring.com>
2020-12-27 00:20:50 -0500

    Cleaned up variable names, comments
    Modified main KBBackup.py to take the zendesk url as an argument just to test if we are getting through with the backup since I am having trouble with the restore

--- src/KBBackup.py
+++ src/KBBackup.py
@@ -1,8 +1,8 @@
-'''
-Created on Oct 16, 2015
-
-@author: Elaine
-'''
+#
+#Created on Oct 16, 2015
+#
+#@author: Elaine
+#
 
 import os
 import datetime
@@ -12,102 +12,109 @@ import csv
 
 
 
-'''Welcome message'''
+#Welcome message
 print('********************************************************************')
 print('Welcome to the Zendesk knowledgebase backup script.')
 print('Press any key to continue:')
 print('********************************************************************')
 input()
 
-''' **************** Nullify agent username and pw for security '''
-agentUsername = 'null'
-
-
-''' **************** Getting Zendesk agent login credentials from the user'''
-'''agentUsername = input('Please enter Zendesk agent username:')'''
-'''agentPassword = input('Please enter Zendesk agent password:')'''
-
-''' Compiling credentials'''
-'''credentials = agentUsername, agentPassword'''
-
-''' **************** Getting ready to pull down the backup'''
-''' set up session variable'''
+#**************** set up session variable
 session = requests.Session()
-'''session.auth = credentials'''
 
-'''set up zendesk url, language'''
-zendesk = 'https://miteship.zendesk.com'
-language = 'en-US'
+#**************** Nullify agent username and pw for security
+#agent_username = 'null'
 
-'''pull down date, compile backup path, mkdir if not present'''
+#**************** Getting Zendesk agent login credentials from the user
+#agent_username = input('Please enter Zendesk agent username:')
+#agent_password = input('Please enter Zendesk agent password:')
+
+# Compiling credentials
+#credentials = agent_username, agent_password
+
+#**************** Getting ready to pull down the backup
+#session.auth = credentials
+
+#set up zendesk url, language
+print('Enter Zendesk name:', end='')
+zendeskname = input()
+zendesk = 'https://'+ zendeskname + '.zendesk.com'
+print('Zendesk URL is', zendesk, '. Is this correct? [y/n]')
+url_confirmation = input()
+if (url_confirmation.lower() != 'y'):
+    print('OK. Goodbye!')
+    exit()
+    
+#pull down date, compile backup path, mkdir if not present
 date = datetime.date.today()
-backup_path = os.path.join(str(date), language)
+backup_path = os.path.join(str(date), zendeskname)
 if not os.path.exists(backup_path):
     os.makedirs(backup_path)
   
-''' set up the log'''
+#set up the log
 log = []
-    
-''' set up the endpoint, this is what is actually used to pull down the content'''
+
+#set up the endpoint, this is what is actually used to pull down the content
+language = 'en-US'
 endpoint = zendesk + '/api/v2/help_center/en-us/articles.json'.format(locale=language.lower()) + '?include=users,sections,categories'
 
 print('Press any key to start the backup. This will take a few moments.')
 input()
 
-''' **************** Posting the actual request for backup'''
+#**************** Posting the actual request for backup
 response = session.get(endpoint)
 
-'''Page through all the articles and keep retrieving until there are no more pages'''
+#Page through all the articles and keep retrieving until there are no more pages
 articleCount = 0
 while endpoint:
     
-    '''get the endpoint in json format'''
+    #get the endpoint in json format
     response = session.get(endpoint)
      
-    '''exit if error'''
+    #exit if error
     if response.status_code != 200:
         print('Failed to retrieve articles with error {}'.format(response.status_code))
         exit()
          
-    '''Format result into a Python dictionary '''
+    #Format result into a Python dictionary
     data = response.json()
     
-    '''Print article titles for reference'''
+    #Print article titles for reference
     sectionID = 0
     articleID = 0
     categoryID = 0
      
-    '''Iterate through articles '''
+    #Iterate through articles
     for article in data['articles']:
          
-        ''' Get category and section ID and title '''
-        sectionName = 'null'
-        categoryName = 'null'
+        # Get category and section ID and title
+        section_name = 'null'
+        category_name = 'null'
         for section in data['sections']:
             if ( section['id'] == article['section_id'] ):
-                sectionName = section['name']
+                section_name = section['name']
                 for category in data['categories']:
                     if ( category['id'] == section['category_id']):
-                        categoryName = category['name']
+                        category_name = category['name']
           
-        articleName = article['title']
-        categoryTitle = '<em><strong>CATEGORY:</strong> ' + categoryName + '</em>'
-        sectionTitle = '<em><strong>SECTION:</strong> ' + sectionName + '</em>'
-        articleTitle = '<h1>' + articleName + '</h1>'
+        article_name = article['title']
+        categoryTitle = '<em><strong>CATEGORY:</strong> ' + category_name + '</em>'
+        sectionTitle = '<em><strong>SECTION:</strong> ' + section_name + '</em>'
+        articleTitle = '<h1>' + article_name + '</h1>'
         articleFilename = '{id}.html'.format(id=article['id'])
          
         fileHandle = open(os.path.join(backup_path, articleFilename), mode='w', encoding='utf-8')
              
         with fileHandle as f:
-            ''' FRONT MATTER'''
+            # FRONT MATTER
             f.write('<!DOCTYPE html>\n')
             f.write('<html>\n')
             f.write('<head>\n')
             f.write('<meta charset="UTF-8"/>')
-            f.write('<title>' + articleName + '\n</title>')
+            f.write('<title>' + article_name + '\n</title>')
             f.write('</head>\n')
             
-            ''' BODY '''
+            # BODY
             f.write('<body>\n')
             f.write(articleTitle)
             f.write('<br>')
@@ -118,24 +125,24 @@ while endpoint:
             f.write(article['body'])
             f.write('</body>\n')
             
-            ''' END HTML '''
+            # END HTML
             f.write('</html>\n')
 
          
         ''' this was giving the script trouble bc command cannot display non
              unicode characters. comment out for now until we figure out how to
              reformat those pesky special characters. '''
-        '''print(articleName)'''
+        #print(article_name)'''
                  
-        ''' count how many articles we wrote to file'''
+        #count how many articles we wrote to file
         articleCount = articleCount + 1
         print('Article #', articleCount, 'written to file')
-        log.append((articleFilename, categoryName, sectionName, articleName))
+        log.append((articleFilename, category_name, section_name, article_name))
      
-        '''page through the results until next page is null'''
+        #page through the results until next page is null
         endpoint = data['next_page']
 
-''' Write log to CSV'''
+# Write log to CSV
 with open(os.path.join(backup_path, '_log.csv'), mode='wt', encoding='utf-8') as f:
     writer = csv.writer(f)
     writer.writerow( ('File', 'Category', 'Section', 'Title') )
--- src/Test.py
+++ src/Test.py
@@ -1,149 +1,77 @@
 
-'''
-Created on December 19, 2020
-@author: Elaine Chen
+#
+#Created on December 19, 2020
+#@author: Elaine Chen
+#
+#This is a sandbox
+#
 
-This python script iterates through a Zendesk help center and
-exports all articles into a CSV file.
-'''
 import os
 import datetime
 import requests
 import copy
 import csv
 
+print('*****      *** ')
+print('*         *   *')
+print('*****     *')
+print('*         *   *')
+print('***** *    ***   *')
 
 
-'''Welcome message'''
+
+
+'''
+#Welcome message
 print('********************************************************************')
-print('Welcome to the Zendesk knowledgebase backup script.')
+print('This is a sandbox for learning python.')
 print('Press any key to continue:')
 print('********************************************************************')
 input()
 
-''' **************** Nullify agent username and pw for security '''
+#**************** Nullify agent username and pw for security
 agentUsername = 'null'
+agentPassword = 'null'
 
+#**************** Getting Zendesk agent login credentials from the user
+agentUsername = input('Please enter Zendesk agent username:')
+agentPassword = input('Please enter Zendesk agent password:')
 
-''' **************** Getting Zendesk agent login credentials from the user'''
-'''agentUsername = input('Please enter Zendesk agent username:')'''
-'''agentPassword = input('Please enter Zendesk agent password:')'''
+# Compiling credentials
+credentials = agentUsername, agentPassword
 
-''' Compiling credentials'''
-'''credentials = agentUsername, agentPassword'''
-
-''' **************** Getting ready to pull down the backup'''
-''' set up session variable'''
+#**************** Getting ready to pull down the backup
+#set up session variable
 session = requests.Session()
-'''session.auth = credentials'''
+session.auth = credentials
 
-'''set up zendesk url, language'''
-zendesk = 'https://miteship.zendesk.com'
-language = 'en-US'
+#set up zendesk url, language
+zendesk = 'https://tuftsecenter.zendesk.com'
+locale = 'en-US'
 
-'''pull down date, compile backup path, mkdir if not present'''
+#pull down date, compile backup path, mkdir if not present
 date = datetime.date.today()
-backup_path = os.path.join(str(date), language)
+backup_path = os.path.join(str(date), locale)
 if not os.path.exists(backup_path):
     os.makedirs(backup_path)
   
-''' set up the log'''
+#set up the log
 log = []
     
-''' set up the endpoint, this is what is actually used to pull down the content'''
-endpoint = zendesk + '/api/v2/help_center/en-us/articles.json'.format(locale=language.lower()) + '?include=users,sections,categories'
 
-print('Press any key to start the backup. This will take a few moments.')
-input()
+#Try to make a category
+headers = {
+    'Content-Type': 'application/json',
+}
 
-''' **************** Posting the actual request for backup'''
-response = session.get(endpoint)
-
-'''Page through all the articles and keep retrieving until there are no more pages'''
-articleCount = 0
-while endpoint:
-    
-    '''get the endpoint in json format'''
-    response = session.get(endpoint)
-     
-    '''exit if error'''
-    if response.status_code != 200:
-        print('Failed to retrieve articles with error {}'.format(response.status_code))
-        exit()
-         
-    '''Format result into a Python dictionary '''
-    data = response.json()
-    
-    '''Print article titles for reference'''
-    sectionID = 0
-    articleID = 0
-    categoryID = 0
-     
-    '''Iterate through articles '''
-    for article in data['articles']:
-         
-        ''' Get category and section ID and title '''
-        sectionName = 'null'
-        categoryName = 'null'
-        for section in data['sections']:
-            if ( section['id'] == article['section_id'] ):
-                sectionName = section['name']
-                for category in data['categories']:
-                    if ( category['id'] == section['category_id']):
-                        categoryName = category['name']
-          
-        articleName = article['title']
-        categoryTitle = '<em><strong>CATEGORY:</strong> ' + categoryName + '</em>'
-        sectionTitle = '<em><strong>SECTION:</strong> ' + sectionName + '</em>'
-        articleTitle = '<h1>' + articleName + '</h1>'
-        articleFilename = '{id}.html'.format(id=article['id'])
-         
-        fileHandle = open(os.path.join(backup_path, articleFilename), mode='w', encoding='utf-8')
-             
-        with fileHandle as f:
-            ''' FRONT MATTER'''
-            f.write('<!DOCTYPE html>\n')
-            f.write('<html>\n')
-            f.write('<head>\n')
-            f.write('<meta charset="UTF-8"/>')
-            f.write('<title>' + articleName + '\n</title>')
-            f.write('</head>\n')
-            
-            ''' BODY '''
-            f.write('<body>\n')
-            f.write(articleTitle)
-            f.write('<br>')
-            f.write(categoryTitle)
-            f.write('<br>')
-            f.write(sectionTitle)
-            f.write('<br>')
-            f.write(article['body'])
-            f.write('</body>\n')
-            
-            ''' END HTML '''
-            f.write('</html>\n')
-
-         
-        ''' this was giving the script trouble bc command cannot display non
-             unicode characters. comment out for now until we figure out how to
-             reformat those pesky special characters. '''
-        '''print(articleName)'''
-                 
-        ''' count how many articles we wrote to file'''
-        articleCount = articleCount + 1
-        print('Article #', articleCount, 'written to file')
-        log.append((articleFilename, categoryName, sectionName, articleName))
-     
-        '''page through the results until next page is null'''
-        endpoint = data['next_page']
-
-''' Write log to CSV'''
-with open(os.path.join(backup_path, '_log.csv'), mode='wt', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow( ('File', 'Category', 'Section', 'Title') )
-    for article in log:
-        writer.writerow(article)
+data = '{"category": {"position": 2, "locale": "en-us", "name": "Super Hero Tricks", "description": "This category contains a collection of super hero tricks"}}'
 
 
-     
-     
+response = requests.post('https://tuftsecenter.zendesk.com/api/v2/help_center/categories.json', headers, data, auth=(credentials))
+
+https://tuftsecenter.zendesk.com/api/v2/help_center/categories.json \
+  -d '{"category": {"position": 1, "locale": "en-us", "name":  "Super Hero Tricks",\
+  "description": "This category contains a collection of super hero tricks"}}' \
+  -v -u agentUsername:agentPassword -X -POST -H "Content-Type: application/json"
+'''
+
